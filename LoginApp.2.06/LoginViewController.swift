8dd9efcbcf0c9ef518ad460c96fbf78dd54f9cde
Alexander6Shevtsov <axrsvr@gmail.com>
2024-05-16 09:03:00 +0300

    Initial Commit

---
+++ LoginApp.2.06/LoginViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  LoginApp.2.06
+//
+//  Created by Alexander Shevtsov on 14.05.2024.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+    
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    private let user = "User"
+    private let password = "123"
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        welcomeVC.user = user
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        guard userNameTextField.text == user, passwordTextField.text == password else {
+            showAlert(
+                title: "Invalid login or password",
+                message: "Please, enter correct login and password",
+                textField: passwordTextField
+            )
+            return false
+        }
+        return true
+    }
+    
+    @IBAction func forgotRegisterDate(_ sender: UIButton) {
+        sender.tag == 0
+        ? showAlert(title: "Oops!", message: "Your name is \(user)")
+        : showAlert(title: "Oops!", message: "Your password is \(password)")
+    }
+    
+    private func showAlert(title: String, message: String, textField: UITextField? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            textField?.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
---
+++ LoginApp.2.06/WelcomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  WelcomeViewController.swift
+//  LoginApp.2.06
+//
+//  Created by Alexander Shevtsov on 14.05.2024.
+//
+
+import UIKit
+
+final class WelcomeViewController: UIViewController {
+    
+    @IBOutlet var welcomeLabel: UILabel!
+    
+    var user = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        welcomeLabel.text = "Welcome, \(user)!"
+    }
+    
+}
